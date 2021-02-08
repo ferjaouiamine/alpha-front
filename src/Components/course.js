4de4ed167f64f0e72925ab98ed01ba29149d95fc
AnisSfar <anis.sfar98@gmail.com>
2021-02-08 14:39:53 +0100

    changing path

--- src/Components/course.js
+++ src/Components/course.js
@@ -31,7 +31,7 @@ const CourseCard = (params) => {
   useEffect(() => {
     const getCourses = async () => {
       try {
-        const coursesRes = await Axios.get("http://localhost:3001/api/courses");
+        const coursesRes = await Axios.get("http://37.59.204.215:3001/api/courses");
         setCoures((await coursesRes).data.data);
       } catch (err) {
         err && setError(err);
