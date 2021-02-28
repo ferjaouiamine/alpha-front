208e8ec62d581db476557d1c34dff4ccd06f738b
AnisSfar <anis.sfar98@gmail.com>
2021-02-28 16:58:44 +0100

    url

--- src/Components/addCourse.js
+++ src/Components/addCourse.js
@@ -10,7 +10,7 @@ import { Alert } from 'antd';
 const { Option } = Select;
 const AddCourse = () => {
   axios.create({
-    baseURL: `https://localhost:3001/api/`,
+    baseURL: `https://37.59.204.215:3001/api/`,
   });
 
   let history = useHistory();
@@ -36,7 +36,7 @@ const AddCourse = () => {
 
       console.log(newCourse);
       const course = await axios.post(
-        "https://localhost:3001/api/course",
+        "https://37.59.204.215:3001/api/course",
         newCourse
       );
       console.log(course);
@@ -68,7 +68,7 @@ const AddCourse = () => {
   const addFiles = {
     name: "file",
     multiple: true,
-    action: "https://localhost:3001/api/upload/uploadFile",
+    action: "https://37.59.204.215:3001/api/upload/uploadFile",
     beforeUpload: (file) => {
       if (file.type !== "application/pdf" && file.type !== "video/mp4") {
         return message.error(`${file.name} type de fichier non autorisé.`);
